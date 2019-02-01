e6e54a136a23948a8b25870ce462ec728804e6a0
Yashvardhan Mathur <work.yvm@gmail.com>
2019-02-01 16:40:02 +0530

    Seeded data visible

--- seed/productSeed.js
+++ seed/productSeed.js
@@ -22,7 +22,7 @@ var products = [
     price:300
   }),
 
-    new product({
+/*    new product({
     imagePath:'https://upload.wikimedia.org/wikipedia/en/8/85/Captain_Marvel_poster.jpg',
     title:'Captain Marvel',
     description:'Captain Marvel gets caught in the middle of a galactic war between two alien races.',
@@ -34,21 +34,21 @@ var products = [
     title:'Avengers:Infinity War',
     description:'Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle their most powerful enemy yet -- the evil Thanos. On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality. The fate of the planet and existence itself has never been more uncertain as everything the Avengers have fought for has led up to this moment.',
     price:300
-  }),
+  }),*/
 
     new product({
     imagePath:'https://upload.wikimedia.org/wikipedia/en/1/1c/Spider-Man_Far_From_Home_poster.jpeg',
     title:'Spider-Man: Far From Home',
     description:"Peter Parker's relaxing European vacation takes an unexpected turn when Nick Fury suddenly shows up in his hotel room. Parker soon finds himself donning the Spider-Man suit to help Fury stop the evil Mysterio from wreaking havoc across the continent.",
     price:300
-  }),
+  })
 
-    new product({
+/*    new product({
     imagePath:'https://upload.wikimedia.org/wikipedia/en/f/fd/How_to_Train_Your_Dragon_3_poster.png',
     title:'How To Train Your Dragon : The Hidden World',
     description:" Now chief and ruler of Berk alongside Astrid, Hiccup has created a gloriously chaotic dragon utopia. When the sudden appearance of female Light Fury coincides with the darkest threat their village has ever faced, Hiccup and Toothless must leave the only home they’ve known and journey to a hidden world thought only to exist in myth",
     price:300
-  }),
+  }),*/
 
 ];
 var done = 0;
